26a61773fcc661a5b242c3178b4f51b77e0b289b
Julia Zwierko (WFiIS) <114949901+juliazverko@users.noreply.github.com>
2022-11-27 23:59:59 +0100

    Update program2.c

--- program2.c
+++ program2.c
@@ -1,3 +1,18 @@
+/*
+Napisz program, który pobierze od użytkownika liczbę całkowitą i wypisze jej wszystkie dzielniki (większe od 1). 
+Jeśli jest to liczba pierwsza, program powinien o tym poinformować. Przykład uruchomienia programu z liczbą, która nie jest pierwsza:
+      Podaj liczbe: 100
+      Dzielniki: 2 4 5 10 20 25 50 100
+Przykład uruchomienia programu z liczbą pierwszą:
+      Podaj liczbe: 101
+      Dzielniki: 101
+      Liczba 101 jest pierwsza.
+      
+UWAGA!!!
+Ten kod nie jest kompletnym rozwiązaniem zadania, a jedynie pokazuje logikę rozwiązania na określonych liczbach. 
+Aby rozwiązać zadanie do końca, użyj zmiennych typu całkowego.
+*/
+
 #include <stdio.h>
 #include <math.h> 
 
