9c6beebb542130434498aa1b77844be10d5169b9
Julia Zwierko <114949901+juliazverko@users.noreply.github.com>
2022-11-28 13:20:00 +0100

    Update program4.c

--- program4.c
+++ program4.c
@@ -15,25 +15,23 @@ Podaj wysokosc: 4
 
 int main(void)
 {
-
     int n;
     do 
     {
-        printf("wpisz liczbe n dla oznaczenia wysokoscu trojkata: \n");
-        scanf("%d", &n);
-        printf("wpisana liczba wysokosci: %d\n",n);
+    printf("wpisz liczbe n dla oznaczenia wysokoscu trojkata: \n");
+    scanf("%d", &n);
+    printf("wpisana liczba wysokosci: %d\n",n);
     }
     while(n<1);
-
 
     int b;
     for(int i=0; i<n; i++)
     {
         for(int j=0; j<n-i-1; j++)
-            printf(" ");  
+        printf(" ");  
 
         for(int j=0; j<((i*2)+1); j++)
-            printf("*");
+        printf("*");
 
         printf("\n");
     }
