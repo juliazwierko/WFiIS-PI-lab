9cd8d8e3b95549499296fae049e2ce025482fb5b
Julia Zwierko <114949901+juliazverko@users.noreply.github.com>
2022-11-28 13:18:18 +0100

    Update program3.c

--- program3.c
+++ program3.c
@@ -12,13 +12,12 @@ int main(void)
     double n = 2.0; 
     double suma = 1.0; 
     double b = 10.0;
-    
+  
     while(suma<b)
     {   
         suma+=1/n;
         n++;
     }
-    
     printf("Konieczna suma wynosi: %.10f\n", suma);
     printf("ilosc el. wynosi: %d\n", (int)(n-1));
 
