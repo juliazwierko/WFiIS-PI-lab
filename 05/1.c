76b4a0695a93e734690e4435f7768514495bcb9c
Julia Zwierko <114949901+juliazwierko@users.noreply.github.com>
2022-12-16 13:54:23 +0100

    Create 1.c

---
+++ 05/1.c
@@ -0,0 +1,9 @@
+//*
+Napisz program, który w main() pobierze od użytkownika liczbę całkowitą a oraz liczbę bitów ile, które mają zostać ustawione na 1 (zaczynając od najmłodszego). Napisz funkcję zgodną z prototypem:
+      void ustaw(int * , int);
+która przy użyciu operatorów bitowych oraz odpowiedniej maski ustawi na 1 bity o pozycjach 0, 1, ..., ile-1 w wartości zmiennej a. Proszę przetestować działanie dla przykładowych wartości w main() oraz wypisać zmienną a przed i po wywołaniu funkcji zerowania, np.:
+      Podaj liczbe: 41
+      Ile najmlodszych bitow ma zostac ustawionych na 1? 4
+      Przed wywolaniem funkcji: a = 41
+      Po wywolaniu funkcji: a = 47
+*//
