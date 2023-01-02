99a8da4d6cd90c2b418139dd4835ad8753bb161f
Julia Zwierko <114949901+juliazwierko@users.noreply.github.com>
2023-01-02 23:56:34 +0100

    Update ***3.c

--- 06/***3.c
+++ 06/***3.c
@@ -16,24 +16,7 @@
     29, adres: 0x7ffdfe793784
     Najmniejszy element jest rowny 22 i znajduje sie pod adresem: 0x7ffdfe793774
 */
-/*
-    Napisz program, który znajduje wskaźnik do najmniejszego elementu tablicy dziesięciu losowych liczb 
-    całkowitych z zakresu [0, 100] (instrukcja rand()%101). W tym programie nie można deklarować innych 
-    zmiennych niż wskaźniki (nazwa tablicy też jest wskaźnikiem).
-    Dla każdego elementu tablicy wypisz jego wartość i adres. Po znalezieniu wskaźnika do najmniejszego
-    elementu również wypisz wartość tego elementu oraz jego adres, np.:
-    70, adres: 0x7ffdfe793760
-    91, adres: 0x7ffdfe793764
-    90, adres: 0x7ffdfe793768
-    55, adres: 0x7ffdfe79376c
-    66, adres: 0x7ffdfe793770
-    22, adres: 0x7ffdfe793774 
-    47, adres: 0x7ffdfe793778
-    42, adres: 0x7ffdfe79377c
-    67, adres: 0x7ffdfe793780
-    29, adres: 0x7ffdfe793784
-    Najmniejszy element jest rowny 22 i znajduje sie pod adresem: 0x7ffdfe793774
-*/
+
 #include <stdio.h> 
 #include <stdlib.h>
 #include <time.h> 
