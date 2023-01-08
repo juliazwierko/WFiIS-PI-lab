579a33f9bfbe1a77a1bdc8f5adfd86b7941314ff
Julia Zwierko <114949901+juliazwierko@users.noreply.github.com>
2023-01-08 23:26:56 +0100

    Update 4.c

--- 06/4.c
+++ 06/4.c
@@ -2,13 +2,13 @@
 Utwórz w main() tablicę char tab[] = "0000";, w której będzie przechowywana liczba binarna zapisana w systemie U1.
 Zaprogramuj trzy funkcje:
 
-void losuj(char *t, int n); - wypełnia wejściową tablicę losowymi bitami, zapisanymi w formie znaków. 
+1)void losuj(char *t, int n); - wypełnia wejściową tablicę losowymi bitami, zapisanymi w formie znaków. 
 Parametr n oznacza liczbę bitów do wylosowania.
 
-void pisz(char *t); - wypisuje tablicę wejściową traktując ją jako łańcuch (bez użycia pętli). 
+2)void pisz(char *t); - wypisuje tablicę wejściową traktując ją jako łańcuch (bez użycia pętli). 
 
-Trzecia funkcja powinna zwracać (nie wypisywać) wartość dziesiętną wylosowanej liczby
-binarnej w U1. Wartość potęgi 2k można obliczyć przy pomocy funkcji z biblioteki
+3)Trzecia funkcja powinna zwracać (nie wypisywać) wartość dziesiętną wylosowanej liczby
+binarnej w U1. Wartość potęgi 2^k można obliczyć przy pomocy funkcji z biblioteki
 matematycznej: pow(2, k). Funkcja ta wymaga kompilacji programu z opcją -lm
 W main() przetestuj kolejno działanie swoich trzech funkcji, przekazując do nich wskaźnik do pierwszego elementu tablicy tab.
 
@@ -20,7 +20,7 @@ Wylosowana liczba binarna w U1 ma wartosc dziesietna: -3
 #include <stdio.h> 
 #include <stdlib.h>
 #include <time.h> 
-#include <math.h> // gcc -g -Wall -lm
+#include <math.h> // gcc -g -Wall -lm wpisujemy w terminale 
 
 void losuj(char *tab, int n)
 {
@@ -59,7 +59,7 @@ int main()
     
     losuj(tab, 4);//losowanie
     pisz(tab); //wypisywanie
-    printf("\nWylosowana liczba binarna w U1 ma wartosc dziesietna: %d\n", dzisietnyzapis(tab, 4));
+    printf("\nWylosowana liczba binarna w U1 ma wartosc dziesietna: %d\n", dzisietnyzapis(tab, 4)); //dziesiętny zapis;
 
     return 0;
 }
