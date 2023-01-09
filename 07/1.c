6278b0a3bc804a45acf0f47f383858caf5e65cbf
Julia Zwierko <114949901+juliazwierko@users.noreply.github.com>
2023-01-09 13:03:09 +0100

    Create 1.c

---
+++ 07/1.c
@@ -0,0 +1,8 @@
+Napisz program, który wypisuje wszystkie parametry wywołania programu podane przy uruchomieniu
+oraz ich liczbę, np.:
+Komenda uruchomienia: ./a.out raz dwa
+Na ekranie wyświetla się:
+Podano 3 parametry wywolania programu:
+--> argv[0] = ./a.out
+--> argv[1] = raz
+--> argv[2] = dwa
