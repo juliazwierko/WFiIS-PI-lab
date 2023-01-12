8c00e92d644b4b01eccdd4d71031749cb31883e5
Julia Zwierko <114949901+juliazwierko@users.noreply.github.com>
2023-01-12 14:10:48 +0100

    Update 1.c

--- 07/1.c
+++ 07/1.c
@@ -6,3 +6,21 @@ Podano 3 parametry wywolania programu:
 --> argv[0] = ./a.out
 --> argv[1] = raz
 --> argv[2] = dwa
+
+
+#include "stdio.h"
+#include "stdlib.h"
+int main(int argc, char *argv[]) 
+{
+ int i;
+ printf("Liczba argumentów wiersza poleceń %d \n", argc);
+ printf("Argumenty wiersza poleceń:\n");
+ for (i = 0; i < argc; i++) printf("arg[i] = %s\n", argv[i]);
+ return 0;
+}
+
+Liczba argumentów wiersza poleceń 3 
+Argumenty wiersza poleceń:
+arg[i] = ./test1
+arg[i] = raz
+arg[i] = dwa
