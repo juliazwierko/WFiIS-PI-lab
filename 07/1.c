695af531d9b47c7abf525c6f9d637cbdf1db9d50
Julia Zwierko <114949901+juliazwierko@users.noreply.github.com>
2023-01-12 16:14:32 +0100

    Update 1.c

--- 07/1.c
+++ 07/1.c
@@ -1,3 +1,4 @@
+/*
 Napisz program, który wypisuje wszystkie parametry wywołania programu podane przy uruchomieniu
 oraz ich liczbę, np.:
 Komenda uruchomienia: ./a.out raz dwa
@@ -6,6 +7,7 @@ Podano 3 parametry wywolania programu:
 --> argv[0] = ./a.out
 --> argv[1] = raz
 --> argv[2] = dwa
+*/
 
 #include "stdio.h"
 #include "stdlib.h"
@@ -14,10 +16,14 @@ int main(int argc, char *argv[])
  int i;
  printf("Liczba argumentów wiersza poleceń %d \n", argc);
  printf("Argumenty wiersza poleceń:\n");
- for (i = 0; i < argc; i++) printf("arg[%d] = %s\n",i, argv[i]);
+ for (i = 0; i < argc; i++) printf("arg[%d] = %s\n", i, argv[i]);
  return 0;
 }
+
+/*
+OUTPUT:
 
 arg[0] = ./test1
 arg[1] = raz
 arg[2] = dwa
+8/
